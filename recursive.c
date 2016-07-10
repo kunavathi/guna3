0a79861ab6e61be5641cffc396ade5c04177ca04
kunavathi <gunasaindhu@gmail.com>
2016-07-10 12:19:25 +0530

    finish

---
+++ recursive.c
@@ -0,0 +1,35 @@
+# guna3
+#include<stdio.h>
+thick(int ,int);
+color(int);
+angle(int);
+main()
+{
+int a[10],b[10];
+printf("program for recursive");
+thick(4,2);
+color(3);
+angle(40);
+getch();
+}
+thick(int a,int b)
+{
+if(a<10 && b<10)
+{
+printf("line get thinner&branchlenth is smaller");
+}
+}
+color(int c)
+{
+if(c<5)
+{
+printf("it is coloured like leaf);
+}
+}
+angle(int)
+{
+printf(int d)
+{
+printf("random");
+}
+}
